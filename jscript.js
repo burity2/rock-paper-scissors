3e2c47c6f338fe8ad3c56820b8e28809ad9716ef
burity2 <arthur.rosa66@gmail.com>
2025-04-14 19:04:14 -0300

    i loved doing this and learned a lot!

--- jscript.js
+++ jscript.js
@@ -1,7 +1,39 @@
 let humanScore = 0;
 let computerScore = 0;
 
-function getComputerChoice () {
+const humanScoreTracker = document.getElementById("human-score-tracker")
+const humanScoreUpdater = document.createElement("div")
+
+const machineScoreTracker = document.getElementById("machine-score-tracker")
+const machineScoreUpdater = document.createElement("div")
+
+const bottomText = document.getElementById("bottom-text")
+const roundUpdater = document.createElement("div")
+
+let getHumanChoice = ""
+const humanChoice = document.querySelector(".options");
+
+humanChoice.addEventListener ("click", (event) => {
+    let target = event.target;
+
+    switch(target.id) {
+        case "brick-button":
+            getHumanChoice = "Brick"
+            //console.log(getHumanChoice);
+            break;
+        case "papyrus-button":
+            getHumanChoice = "Papyrus"
+           //console.log(getHumanChoice);
+            break;
+        case "khopesh-button":
+            getHumanChoice = "Khopesh"
+           //console.log(getHumanChoice);
+            break;
+    }
+});
+//console.log(getHumanChoice)
+
+function getComputerChoice() {
     const choice = Math.floor(Math.random() * 3);
     if (choice === 2) {
         return "Khopesh";
@@ -12,6 +44,128 @@ function getComputerChoice () {
     }
 }
 
+// const playRound = document.querySelector(".options");
+// playRound.addEventListener ("click", round)
+
+function round (playerCall = getHumanChoice, computerCall) {
+    computerCall = getComputerChoice()
+
+    machineScoreTracker.textContent = "score: " + computerScore
+        humanScoreTracker.textContent = "score: " + humanScore
+    
+    if (getHumanChoice === computerCall) {
+        bottomText.textContent = "Tie! Both chose " + getHumanChoice
+       
+
+        //console.log("tie")
+        //return "Tie! Both chose " + playerCall
+    } else if (
+        (getHumanChoice === "Brick" && computerCall === "Papyrus") ||
+        (getHumanChoice === "Papyrus" && computerCall === "Khopesh") ||
+        (getHumanChoice === "Khopesh" && computerCall === "Brick") 
+        ) {
+        computerScore++
+        machineScoreTracker.textContent = "score: " + computerScore
+        bottomText.textContent = "You lost! The Sphynx's " + computerCall + " beats your " + getHumanChoice
+
+
+        //console.log()
+        //return "You lost! " + computerCall + " beats " + playerCall;
+    } else {
+        humanScore++
+        humanScoreTracker.textContent = "score: " + humanScore
+        bottomText.textContent = "You Win! Your " + getHumanChoice + " beats the Sphynx's " + computerCall
+        
+        //console.log("win")
+        //return "You win! " + playerCall + " beats " + computerCall;
+    }
+}
+
+const playGame = document.querySelector(".options")
+playGame.addEventListener ("click", gamePlay)
+
+function gamePlay () {
+    round()
+
+    if (humanScore === 5) {
+        bottomText.textContent = "Victory! You won, human! Try again?"
+        //console.log("humans win, " + "humans:" + humanScore + "machines" + computerScore)
+        humanScore = 0
+        computerScore = 0
+        machineScoreTracker.textContent = "Defeat!"
+        humanScoreTracker.textContent = "Victory!"
+
+
+    } else if (computerScore === 5) {
+        bottomText.textContent = "Game over! Run for your life, human! Retry in your next life?"
+        //console.log("humans lost, " + "humans:" + humanScore + "machines" + computerScore)
+        humanScore = 0
+        computerScore = 0
+        machineScoreTracker.textContent = "Victory!"
+        humanScoreTracker.textContent = "Defeat!"
+}
+}
+
+
+```
+function playRound(playerCall, computerCall) {
+    let computerCall = getComputerChoice()
+
+    if (playerCall === computerCall) {
+        return "Tie! Both chose " + playerCall
+    } else if (
+        (playerCall === "Brick" && computerCall === "Papyrus") ||
+        (playerCall === "Papyrus" && computerCall === "Khopesh") ||
+        (playerCall === "Khopesh" && computerCall === "Brick") 
+        ) {
+        computerScore++ 
+        return "You lost! " + computerCall + " beats " + playerCall;
+    } else {
+        humanScore++
+        return "You win! " + playerCall + " beats " + computerCall;
+    }
+}
+
+const endRound = document.querySelector(".options");
+endRound.addEventListener("mouseup", finishGame)
+
+function finishGame () {
+    if (humanScore === 5) {
+        console.log("humans win, " + "humans:" + humanScore + "machines" + computerScore)
+        humanScore = 0
+        computerScore = 0
+
+    } else if (computerScore === 5) {
+        console.log("humans lost, " + "humans:" + humanScore + "machines" + computerScore)
+        humanScore = 0
+        computerScore = 0
+    
+    } else {
+        return " "
+    }
+}
+
+function playGame () {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    while (humanScore < 5 && computerScore < 5) {
+        //const humanMove == getHumanChoice;
+        const computerMove = getComputerChoice();
+
+        const roundResult = playRound(getHumanChoice, computerMove);
+        console.log(roundResult);
+
+        if (roundResult.includes("You win")) humanScore++;
+        else if (roundResult.includes("You lost")) computerScore++;
+
+        console.log("Score: Human " + humanScore + " , Computer " + computerScore);
+    }
+
+    
+}
+
+
 function getHumanChoice () {
     let sign = prompt("Take your move! (Brick, Papyrus or Khopesh)")
     if (sign.toLowerCase() === "khopesh") {
@@ -21,23 +175,6 @@ function getHumanChoice () {
     } else if (sign.toLowerCase() === "brick") {
         return "Brick";
     }
-}
-
-function playRound(humanPlay, computerPlay) {
-    if (humanPlay === computerPlay) {
-        return "Tie! Both chose " + humanPlay
-    } else if (
-        (humanPlay === "Brick" && computerPlay === "Papyrus") ||
-        (humanPlay === "Papyrus" && computerPlay === "Khopesh") ||
-        (humanPlay === "Khopesh" && computerPlay === "Brick") 
-        ) {
-        computerScore++ 
-        return "Human lost! " + computerPlay + " beats " + humanPlay;
-    } else {
-        humanScore++
-        return "Human wins! " + humanPlay + " beats " + computerPlay;
-    }
-}
 
 function playGame () {
     let humanScore = 0;
@@ -64,7 +201,4 @@ function playGame () {
 }
 
 console.log(playGame());
-
-
-
-
+```
